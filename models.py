1cd7ca7c51a485421ac5e8b628fcd6037ca945e2
rafiqahsns <rafiqhsns@gmail.com>
2020-05-15 22:09:53 +0700

    migration

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
