53778952d9b98d2fae43c96cc83ba4a0b98cd85c
rafiqahsns <rafiqhsns@gmail.com>
2020-05-15 13:46:51 +0700

    add greetings

--- app.py
+++ app.py
@@ -23,16 +23,13 @@ handler = WebhookHandler(
            default=os.environ.get('LINE_CHANNEL_SECRET'))
 )
 
-
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
 
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
 
     # handle webhook body
     try:
@@ -40,18 +37,20 @@ def callback():
     except InvalidSignatureError:
         abort(400)
 
-
     return 'OK'
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
-
-
+    if event.message.text == "halo":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="hai qoh")
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
