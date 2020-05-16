7814e042d44d71740ca210dbfff24963ece1d561
rafiqahsns <rafiqhsns@gmail.com>
2020-05-16 13:12:08 +0700

    fix bug

--- app.py
+++ app.py
@@ -128,9 +128,9 @@ def handle_text_message(event):
             )
         else:
             line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=(bday)
-                TextSendMessage(text=(events)
+                event.reply_token,[
+                TextSendMessage(text=(bday),
+                TextSendMessage(text=(events)]
             )
 
     else:
